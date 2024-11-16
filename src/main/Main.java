f8c67f5cf66823de3cba12a8253a469ee45e0729
Davide Giordano <www.link.1996@hotmail.it>
2024-11-16 20:20:38 +0100

    .gitignore

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,22 +5,24 @@ import language.visitor.EvalExpVisitor;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		/*String txt = "if ( 1 == 1 ) 20 % 3 ; "
-				+ "int a = 20 ; "
-				+ "a = $ a - 1 ; "
-				+ "if ( a > 30 ) 10 * 0 ; else 1 ; "
-				+ "while ( a > 10 ) { a ; a = $ a - 1 ; } "
-				+ "EOP";*/
+
+		/*
+		 * String txt = "if ( 1 == 1 ) 20 % 3 ; "
+		 * + "int a = 20 ; "
+		 * + "a = $ a - 1 ; "
+		 * + "if ( a > 30 ) 10 * 0 ; else 1 ; "
+		 * + "while ( a > 10 ) { a ; a = $ a - 1 ; } "
+		 * + "EOP";
+		 */
 
 		String txt = "int a = 20 ; "
 				+ "while ( a > 10 ) a = $ a - 1 ; "
 				+ "EOP";
-		
+
 		Scanner scanner = new Scanner(txt);
 		Parser parser = new Parser(scanner);
 		EvalExpVisitor v = new EvalExpVisitor();
-		
+
 		SolveInfo info = new SolveInfo(parser, v);
 		info.solve();
 	}
